8f98942e6be9d13f980d95d6aa83c0ff62359cc3
katbi89 <katbi89@gmail.com>
2021-07-07 19:34:29 +0300

    Show All Wish List Products

--- resources/views/livewire/wishlist-count-component.blade.php
+++ resources/views/livewire/wishlist-count-component.blade.php
@@ -1,6 +1,6 @@
 
 <div class="wrap-icon-section minicart">
-    <a href="#" class="link-direction">
+    <a href="{{ route('product.wishlist')}}" class="link-direction">
         <i class="fa fa-shopping-basket" aria-hidden="true"></i>
         <div class="left-info">
             @if(Cart::instance('cart')->count() > 0)
