b906b096786899ef9a9652ee40b685257c567d92
katbi89 <katbi89@gmail.com>
2021-07-10 19:18:03 +0300

    User Show Orders and Order Details

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\User\UserOrderDetailsComponent;
+use App\Http\Livewire\User\UserOrdersComponent;
 use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -69,6 +71,8 @@ Route::get('/thank-you', ThankyouComponent::class)->name('thankyou');
 // For User And Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+    Route::get('/user/orders', UserOrdersComponent::class)->name('user.orders');
+    Route::get('/user/orders/{order_id}', UserOrderDetailsComponent::class)->name('user.orderdetails');
 });
 
 
