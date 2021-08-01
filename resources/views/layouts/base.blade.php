0133fb47a9ee6ae289d40c367e35d8b186ad5535
katbi89 <katbi89@gmail.com>
2021-08-01 14:29:10 +0300

    update logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -152,7 +152,7 @@
 					<div class="mid-section main-info-area">
 
 						<div class="wrap-logo-top left-section">
-							<a href="/" class="link-to-home"><img src="{{ asset('assets/images/logo-top-1.png') }}" alt="mercado"></a>
+							<a href="/" class="link-to-home"><img src="{{ asset('assets/images/logo-top-1.png') }}" style="border-radius:27px; margin-left:25px" width="100" alt="mercado"></a>
 						</div>
 
 						@livewire('header-search-component')
